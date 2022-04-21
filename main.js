9ff63209757a478619af3c5d94402f34d20edc15
Neurs <31346263+Neurs12@users.noreply.github.com>
2022-04-21 08:40:14 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+const {app, BrowserWindow} = require("electron");
+
+const Window = () => {
+    const win = new BrowserWindow({
+        show: false
+    })
+    win.setIcon("icon.ico")
+    win.setBackgroundColor("#202225")
+    win.setMenu(null)
+    win.setTitle("Home - LQDOJ: Le Quy Don Online Judge")
+    win.loadFile("UI/index.html")
+    win.maximize()
+    win.show()
+}
+
+app.whenReady().then(() => {
+    Window()
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
